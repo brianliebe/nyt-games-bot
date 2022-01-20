e73113bc1693db96073e9a467cbf91d1f0e1468c
Brian Liebe <brianjliebe@gmail.com>
2022-01-19 20:33:27 -0500

    Switch to EDT (x3)

--- cogs/members.py
+++ cogs/members.py
@@ -142,7 +142,7 @@ class MembersCog(commands.Cog, name="Normal Members Commands"):
     def get_todays_puzzle(self) -> str:
         arbitrary_date = date(2022, 1, 10)
         arbitrary_date_puzzle = 205
-        todays_date = datetime.now(timezone(timedelta(hours=-5), 'EST'))
+        todays_date = datetime.now(timezone(timedelta(hours=-5), 'EST')).date()
         return str(arbitrary_date_puzzle + (todays_date - arbitrary_date).days)
 
     def get_nickname(self, user_id) -> str:
